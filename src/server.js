b9f2bd29678ae44d9992878342fc2c014dc06b2c
ZulluBalti <ZulluBalti@gmail.com>
2023-11-14 14:03:02 +0500

    update server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
-// export const server = "https://eshop-tutorial.vercel.app/api/v2";
+// export const server = "http://localhost:8000/api/v2";
+export const server = "https://backend-revvin-3c0c4797375d.herokuapp.com/api/v2";
 
 
