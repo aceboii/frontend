8d39db1128abab68cc50e1ebc03b17bb22d437d9
ZulluBalti <ZulluBalti@gmail.com>
2023-11-15 14:29:53 +0500

    update

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-// export const server = "http://localhost:8000/api/v2";
 export const server = "https://backend-revvin-3c0c4797375d.herokuapp.com/api/v2";
 export const SURL = "https://backend-revvin-3c0c4797375d.herokuapp.com:4000"
 
